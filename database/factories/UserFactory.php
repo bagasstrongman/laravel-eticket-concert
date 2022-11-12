d6243ce05ebc3de07a3916cb76aaeddc4fe9fc49
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-11-13 01:02:27 +0700

    feat: initial commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'username' => fake()->unique()->userName,
+            'email' => fake()->unique()->safeEmail(),
+            'language' => fake()->randomElements(['id', 'en'])[0],
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
+        ];
+    }
+}
