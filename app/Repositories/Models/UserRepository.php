a8085d4cca3d0680c9d9140a8216ef7026848651
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-11-14 00:00:46 +0700

    feat: some repository
    add base repositories

---
+++ app/Repositories/Models/UserRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repositories\Models;
+
+use App\Models\User;
+use App\Traits\UploadFile;
+use App\Contracts\Models\UserInterface;
+use Illuminate\Database\Eloquent\Model;
+use App\Repositories\EloquentRepository;
+
+class UserRepository extends EloquentRepository implements UserInterface
+{
+    use UploadFile;
+
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * Base respository constructor
+     * 
+     * @param Model $model
+     */
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+    
+    /**
+     * Create a model.
+     *
+     * @param array $payload
+     * @return Model
+     */
+    public function create(array $payload): ?Model
+    {
+        $model = $this->model->create($payload);
+
+        if (array_key_exists('role', $payload)) {
+            $model->assignRole($payload['role']);
+        } else {
+            // $model->assignRole('user');
+        }
+
+        return $model->fresh();
+    }
+    
+    /**
+     * Update existing model.
+     *
+     * @param int $modelId
+     * @param array $payload
+     * @return Model
+     */
+    public function update(int $modelId, array $payload): bool
+    {
+        $model = $this->findById($modelId);
+
+        if (array_key_exists('role', $payload)) {
+            $model->syncRoles($payload['role']);
+        }
+
+        return $model->update($payload);
+    }
+}
