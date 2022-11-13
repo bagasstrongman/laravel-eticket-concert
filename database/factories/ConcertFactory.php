eac8de2f49ce5a43e077cbbd380cefbe5f86c6a3
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-11-13 10:50:11 +0700

    feat: some database factory
    complete all database factory

---
+++ database/factories/ConcertFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Concert>
+ */
+class ConcertFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'company' => rand(1,10),
+            'name' => fake()->unique()->name,
+            'start_at' => now(),
+            'end_at' => now()
+        ];
+    }
+}
