3ea6261b77bc2b8e4d82df5107ad0fbb12720c46
Sleepy4k <sarahpalastrin@gmail.com>
2022-11-15 11:18:20 +0700

    feat: form request
    add buy form request

---
+++ app/Http/Requests/Api/Buy/StoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Api\Buy;
+
+use App\Http\Requests\ApiRequest;
+
+class StoreRequest extends ApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth('sanctum')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'concert' => ['required','numeric'],
+            'quantity' => ['required','numeric']
+        ];
+    }
+}
