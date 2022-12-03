d86e0b38d4bebe3924c7a96d667c2bf8633d8e96
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-03 18:53:06 +0700

    feat: account form request
    add store validation form request for account controller

---
+++ app/Http/Requests/Api/Profile/Account/StoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Api\Profile\Account;
+
+use App\Rules\LanguageRule;
+use App\Http\Requests\ApiRequest;
+
+class StoreRequest extends ApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth('sanctum')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'username' => ['nullable','string','max:255','unique:users,username'],
+            'email' => ['nullable','string','max:255','unique:users,email','email:dns'],
+            'language' => ['nullable','string','max:255',new LanguageRule],
+            'password' => ['nullable','string','min:8','max:255','confirmed']
+        ];
+    }
+}
