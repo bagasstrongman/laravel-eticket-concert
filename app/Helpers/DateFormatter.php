b28a7d907837bf21beab2d90795fd4d0f1f33586
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-03 18:51:35 +0700

    chore: some helper
    change function documentation

--- app/Helpers/DateFormatter.php
+++ app/Helpers/DateFormatter.php
@@ -2,26 +2,26 @@
 
 use Carbon\Carbon;
 
+/**
+ * Convert date format from Y-m-d to d-m-Y
+ *
+ * @param mixed $date
+ * @return date
+ */
 if (!function_exists('dateYmdToDmy')) {
-    /**
-     * Convert date format from Y-m-d to d-m-Y
-     *
-     * @param mixed $date
-     * @return date
-     */
     function dateYmdToDmy($date)
     {
         return Carbon::parse($date)->format('d-m-Y');
     }
 }
 
+/**
+ * Convert date format from d-m-Y to Y-m-d
+ *
+ * @param mixed $date
+ * @return date
+ */
 if (!function_exists('dateDmyToYmd')) {
-    /**
-     * Convert date format from d-m-Y to Y-m-d
-     *
-     * @param mixed $date
-     * @return date
-     */
     function dateDmyToYmd($date)
     {
         return Carbon::parse($date)->format('Y-m-d');
--- app/Helpers/NullToEmptyString.php
+++ app/Helpers/NullToEmptyString.php
@@ -1,12 +1,12 @@
 <?php
 
+/**
+ * Replace null varible to empty string
+ * 
+ * @param mixed $varible
+ * @return string
+ */
 if (!function_exists('nullToEmptyString')) {
-    /**
-     * Replace null varible to empty string
-     * 
-     * @param mixed $varible
-     * @return string
-     */
     function nullToEmptyString($varible)
     {
         return $varible = $varible === null ? "" : $varible;
