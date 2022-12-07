05df88c10110fcd6a89d76602bfca0a68eb3795d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-07 17:45:16 +0700

    chore: kernel
    clean up code

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\ApiHeaders::class,
+            'jsonify',
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -75,6 +75,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'jsonify' => \App\Http\Middleware\Jsonify::class,
 
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
